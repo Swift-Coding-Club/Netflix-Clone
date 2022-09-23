aac4a67a60a2c9aa1262249825e62125681889b5
양경모 <gaeng2y@gmail.com>
2022-09-24 01:05:30 +0900

    홈 프리뷰

--- Swift-Coding-Club-Netflix-Clone/Swift-Coding-Club-Netflix-Clone/ContentView.swift
+++ Swift-Coding-Club-Netflix-Clone/Swift-Coding-Club-Netflix-Clone/ContentView.swift
@@ -9,8 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            //: main vstack
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    ZStack {
+                        Image("es")
+                            .resizable()
+                            .scaledToFill()
+                        
+                        VStack {
+                            Spacer()
+                            
+                            Group {
+                                Text("N 시리즈")
+                                Text("다 머")
+                                Text("괴물: 제프리 다머 이야기")
+                                Text("불길한, 심리, 공포, 한 시대를 담은 범죄극")
+                            }
+                            
+                            HStack {
+                                Spacer()
+                                
+                                Button {
+                                    print("내가 찜한 콘텐츠")
+                                } label: {
+                                    VStack {
+                                        Image(systemName: "plus")
+                                        Text("내가 찜한 콘텐츠")
+                                    }
+                                }
+                                
+                                Spacer()
+                                
+                                Button {
+                                    print("재생")
+                                } label: {
+                                    HStack {
+                                        Image(systemName: "play.fill")
+                                        Text("재생")
+                                    }
+                                }
+                                
+                                Spacer()
+                                
+                                Button {
+                                    print("정보")
+                                } label: {
+                                    VStack {
+                                        Image(systemName: "info.circle")
+                                        Text("정보")
+                                    }
+                                }
+                                
+                                Spacer()
+                            }
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
 }
 
